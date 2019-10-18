4152151333127c08b7b196e1cf1bceab2241643a
Martin Avagyan <martinavagyan@Martins-MacBook-Pro-2.local>
2019-10-18 15:34:55 +0200

    initial setup

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-# scala-api-test
+## Sample scala api project
+
+Run `sbt update` and `sbt run`, find your api endpoint at `http://localhost:9000/test`
+You should see `Api works` text on your screen
---
+++ src/main/scala/Main.scala
@@ -0,0 +1,33 @@
+
+import akka.actor.ActorSystem
+import akka.http.scaladsl.Http
+import akka.stream.ActorMaterializer
+
+import scala.concurrent.Await
+import scala.util.{Failure, Success}
+
+object Main extends App {
+
+  val host = "0.0.0.0"
+  val port = 9000
+
+  implicit val system: ActorSystem = ActorSystem(name = "testApi")
+  implicit val materializer: ActorMaterializer = ActorMaterializer()
+  import system.dispatcher
+
+  import akka.http.scaladsl.server.Directives._
+  def route = path("test") {
+    get {
+      complete("Api works")
+    }
+  }
+
+  val binding = Http().bindAndHandle(route, host, port)
+  binding.onComplete {
+    case Success(_) => println("Success!")
+    case Failure(error) => println(s"Failed: ${error.getMessage}")
+  }
+
+  import scala.concurrent.duration._
+  Await.result(binding, 3.seconds)
+}
